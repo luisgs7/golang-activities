29ecfd87ec5e76d5db356ff5f1e5de78a466366f
luisgs7 <luisgsilva270@gmail.com>
2022-11-27 12:11:54 -0300

    start app

---
+++ go.mod
@@ -0,0 +1,3 @@
+module activities/hello
+
+go 1.19
---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Go Developers"
+	fmt.Println("Azure for", name)
+	hello()
+}
+
+func hello() {
+	message := "Hey"
+	fmt.Println(message)
+}
